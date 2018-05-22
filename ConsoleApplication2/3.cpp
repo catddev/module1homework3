2f289e08d1917acfab2513fda2be74fe7d263b53
айкеновад <@238_8>
2018-05-22 12:52:09 +0600

    -

--- ConsoleApplication2/3.cpp
+++ ConsoleApplication2/3.cpp
@@ -16,7 +16,7 @@ void main()
 	{
 		// Известно, что X кг конфет стоит A рублей. Определить, сколько стоит 1 кг и Y кг этих же конфет.
 
-		int X=0, A=0, y=0, b=0;
+		int X=0, A=0, y=0, b;
 		printf("X=\t");
 		scanf("%d", &X);
 
@@ -36,17 +36,21 @@ void main()
 		// Решить линейное уравнение A·x + B = 0, заданное своими коэффициентами A и B (коэффициент A не равен 0)
 
 		int A, x=0, B=0;
-		printf("vvedite  A=");
+		printf("vvedite  A=\t");
 		scanf("%d", &A);
-		printf("vvedite  B=");
+		printf("vvedite  B=\t");
 		scanf("%d", &B);
 
 		x = -B / A;
 
-		printf("x=%d\n", x);
+		printf("x=%d\t", x);
 
 
 	}
 
+	else {
+		
+		printf("нет заданий\n");
 
+	}
 }
